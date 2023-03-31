c1df571ceb3a9ed2eab95dd7ec135db3c02d01ed
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2023-03-31 14:05:36 +0200

    Tweak DbQueries for truncated db

--- Pre.Cu.LINQ.Cons/Examples/DbQueries.cs
+++ Pre.Cu.LINQ.Cons/Examples/DbQueries.cs
@@ -23,14 +23,14 @@ public class DbQueries : IExercise
         _dbContext.Products.Where(p => p.Category.Name == "Beverages").Dump("Beverages");
 
         // using variables
-        string customerId = "Alfki";
+        string customerId = "BLONP";
         _dbContext.Orders
             .Where(o => o.Customer.Id == customerId).Dump($"{customerId}'s Orders")
             .Count().Dump($"#{customerId}'s Orders");
 
         int categoryID = 1;
         _dbContext.Products
-            .Where(p => p.Category.Id == categoryID).Dump($"Products of Catogry {categoryID}")
-            .Sum(p => p.UnitPrice).Dump($"Total Price for products of Catogry {categoryID}");
+            .Where(p => p.Category.Id == categoryID).Dump($"Products of Category {categoryID}")
+            .Sum(p => p.UnitPrice).Dump($"Total Price for products of Category {categoryID}");
     }
 }
