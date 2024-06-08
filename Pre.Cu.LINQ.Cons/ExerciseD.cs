d2230c02da459167d2eb01885aaa58f97fa40e67
LoesCc <loes.coucke@student.howest.be>
2024-06-08 15:21:45 +0200

    Solve Exercise D

--- Pre.Cu.LINQ.Cons/ExerciseD.cs
+++ Pre.Cu.LINQ.Cons/ExerciseD.cs
@@ -15,17 +15,44 @@ public class ExerciseD : IExercise
 
     public void Run()
     {
-        /* TODO 1. Toon de productnamen waarvan we minder dan 20 UnitsInStock tellen, toon ook de bijbehorende categorienaam */
+        var products = _dbContext.Products;
+        // products.Dump("all products");
+        var customers = _dbContext.Customers;
+        // customers.Dump("all customers");
+        var employees = _dbContext.Employees;
+        // employees.Dump("all employees");
 
-        /* TODO 2. Toon de namen van de klanten waar in de ContactTitle het woord "sales" (hoofdletterongevoelig) voorkomt en die afkomstig zijn uit UK */
+        /* Toon de productnamen waarvan we minder dan 20 UnitsInStock tellen, toon ook de bijbehorende categorienaam */
+        products.Where(p => p.UnitsInStock < 20).Select(p => new { p.Name, p.UnitsInStock, p.Category }).Dump("Minder dan 20 units in stock");
 
-        /* TODO 3. Toon de namen van de klanten waar in de ContactTitle het woord "sales" (hoofdletterongevoelig) voorkomt en die afkomstig zijn uit
+        /* Toon de namen van de klanten waar in de ContactTitle het woord "sales" (hoofdletterongevoelig) voorkomt en die afkomstig zijn uit UK */
+        customers.Where(c => c.Contact.Title.ToLower().Contains("sales") && c.Address.Country == "UK").Select(c => c.Name).Dump("Sales uit UK");
+
+        /* Toon de namen van de klanten waar in de ContactTitle het woord "sales" (hoofdletterongevoelig) voorkomt en die afkomstig zijn uit
 	     	   UK, Germany of Brazil*/
+        List<string> allowedCountries = new List<string>() { "UK", "Germany", "Brazil" };
+        customers.Where(c => c.Contact.Title.ToLower().Contains("sales") && allowedCountries.Contains(c.Address.Country)).Select(c => c.Name).Dump("Sales uit UK/Duitsland/Brazilië");
 
-        /* TODO	4. Geef voor elke werknemer die een overste heeft (reportsto is ingevuld)
+        /* Geef voor elke werknemer die een overste heeft (reportsto is ingevuld)
 	        de voornaam en familienaam weer alsook hoeveel orders ze hebben behandeld in het meest recente jaar */
+        employees.AsEnumerable()
+            .Where(e => !string.IsNullOrWhiteSpace(e.ReportsTo))
+            .Select(e => new {
+                e.FirstName,
+                e.LastName,
+                AmountOfOrdersTotal = e.Orders.Count,
+                LastYear = e.Orders.Count == 0 ? 0 : e.Orders.Max(o => o.OrderedDate.Year),
+                AmountOfOrdersLastYear = e.Orders.Count(o => o.OrderedDate.Year == (e.Orders.Count == 0 ? 0 : e.Orders.Max(o => o.OrderedDate.Year)))
+            })
+            .Dump("Werknemers met overste: aantal orders");
 
-        /*  TODO 5. Toon de eerste letter en het aantal ProductNames dat met deze letter begint.
+        /*  Toon de eerste letter en het aantal ProductNames dat met deze letter begint.
             We zien enkel de letters waarvoor er minstens 2 producten zijn. */
+        products.AsEnumerable()
+            .GroupBy(p => p.Name.ToLower().First())
+            .Select(group => new { FirstLetter = group.Key, Amount = group.Count() })
+            .Where(group => group.Amount >= 2)
+            .OrderBy(p => p.FirstLetter)
+            .Dump("Eerste letter producten waarvan minstens 2 producten");
     }
 }
